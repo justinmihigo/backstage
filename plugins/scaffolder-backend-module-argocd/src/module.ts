5dab83062edd8c70c311c0acbe30b21d319ae904
justinmihigo <mihigojustin28@gmail.com>
2025-07-07 10:29:41 +0200

    ch: remove the example action.ts

--- plugins/scaffolder-backend-module-argocd/src/module.ts
+++ plugins/scaffolder-backend-module-argocd/src/module.ts
@@ -1,6 +1,5 @@
 import { createBackendModule } from "@backstage/backend-plugin-api";
 import { scaffolderActionsExtensionPoint } from '@backstage/plugin-scaffolder-node/alpha';
-import { createExampleAction } from "./actions/example";
 import { argocdCreateHelmApplication } from "./argocd";
 import { githubWaitLastWorkflow } from "./github";
 import { coreServices } from "@backstage/backend-plugin-api";
@@ -20,7 +19,6 @@ export const scaffolderModule = createBackendModule({
        
       },
       async init({ scaffolderActions, config}) {
-        scaffolderActions.addActions(createExampleAction());
         scaffolderActions.addActions(argocdCreateHelmApplication());
         scaffolderActions.addActions(githubWaitLastWorkflow(config.get('integrations.github')));
       }
